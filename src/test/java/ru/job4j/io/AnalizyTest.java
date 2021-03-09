f4c164a0a5b5aef7b268051a5b710c57b1b57b22
Selesito <selesitomorozov@gmail.com>
2021-03-09 10:42:16 +0400

    3.0. Тестирование IO [#462183]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,8 +1,10 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.FileInputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +12,10 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class AnalizyTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
     public void whenAnalizeCkeck() {
         String source = "sourse.txt";
@@ -22,7 +28,6 @@ public class AnalizyTest {
             int read;
             while ((read = in.read()) != -1) {
                 text.append((char) read);
-
             }
             result.add(text);
         } catch (Exception e) {
@@ -34,4 +39,23 @@ public class AnalizyTest {
         );
     }
 
+    @Test
+    public void whenAnalizeTemporaryFolder() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        Analizy analizy = new Analizy();
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("300 10:59:01");
+        }
+
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("[10:57:01, 10:59:01]"));
+    }
 }
