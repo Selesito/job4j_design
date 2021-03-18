4df572c7643121d03284f0ea6c9e12255a5f1053
Selesito <selesitomorozov@gmail.com>
2021-03-18 11:31:39 +0400

    1. Бот [#462208]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -13,14 +13,26 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str = in.readLine();
+                    String temp = null;
                     while (!(str.isEmpty())) {
                         System.out.println(str);
-                        if (str.contains("Bye")) {
-                            server.close();
+                        if (str.contains("GET")) {
+                            if (str.contains("Exit")) {
+                                temp = "Exit.\r\n\r\n";
+                                server.close();
+                            } else if (str.contains("Hello")) {
+                                temp = "Hello, dear friend.\r\n\r\n";
+                            } else {
+                                String[] rsl = str.split(" ");
+                                rsl = rsl[1].split("=");
+                                temp = "What - " + rsl[1];
+                            }
                         }
+
                         str = in.readLine();
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\"".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(temp.getBytes());
                 }
             }
         }
