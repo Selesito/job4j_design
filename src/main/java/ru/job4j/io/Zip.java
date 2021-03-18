b70c073977efe0db9bdb49ded39622e0e05304dc
Selesito <selesitomorozov@gmail.com>
2021-03-18 11:53:07 +0400

    5.2. Архивировать проект [#462193]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -10,8 +10,17 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 
     public void packFiles(List<File> sources, File target) {
-        for (File source : sources) {
-            packSingleFile(source, target);
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(
+                new FileOutputStream(target)))) {
+            for (File source : sources) {
+                zip.putNextEntry(new ZipEntry(source.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(
+                        new FileInputStream(source))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -27,16 +36,21 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        ArgZip argZip = new ArgZip(args);
-        if (!argZip.valid()) {
-            throw new IllegalArgumentException("There must be 3 parameters!");
-        }
+    public List<File> packList(ArgZip argZip) throws IOException {
         List<Path> path = Search.search(Path.of(argZip.directory()), argZip.exclude());
         List<File> files = new ArrayList();
         for (Path rsl : path) {
             files.add(rsl.toFile());
         }
-        new Zip().packFiles(files, new File("./" + argZip.output()));
+        return files;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ArgZip argZip = new ArgZip(args);
+        if (!argZip.valid()) {
+            throw new IllegalArgumentException("There must be 3 parameters!");
+        }
+        Zip zip = new Zip();
+        new Zip().packFiles(zip.packList(argZip), new File("./" + argZip.output()));
     }
 }
