99a32822575c757e197e0b48765d40b750371775
Selesito <selesitomorozov@gmail.com>
2021-03-22 16:58:32 +0400

    Поиск файлов по критерию [#462204

--- src/main/java/ru/job4j/db/create.sql
+++ src/main/java/ru/job4j/db/create.sql
@@ -1,3 +1,13 @@
+create table roles (
+    id serial primary key,
+    name varchar(2000)
+);
+
+create table rules (
+    id serial primary key,
+    name varchar(2000)
+);
+
 create table users (
     id serial primary key,
     name varchar(2000),
@@ -9,29 +19,17 @@ create table stateItem (
     name varchar(2000)
 );
 
-create table rules (
-    id serial primary key,
-    name varchar(2000),
-	roles_id int references roles(id)
-);
-
-create table roles (
+create table category (
     id serial primary key,
     name varchar(2000)
-);
-
-create table roles_rules (
-    id serial primary key,
-    roles_id int references roles(id),
-    rules_id int references rules(id)
 );
 
 create table item (
     id serial primary key,
     name varchar(2000),
 	users_id int references users(id),
-	category_id int references category(id),
-	stateItem_id int references stateItem(id)
+	stateItem_id int references stateItem(id),
+	category_id int references category(id)	
 );
 
 create table comments_file (
@@ -40,13 +38,14 @@ create table comments_file (
 	item_id int references item(id)
 );
 
-create table category (
-    id serial primary key,
-    name varchar(2000)
-);
-
 create table attachs (
     id serial primary key,
     name varchar(2000),
 	item_id int references item(id)
+);
+
+create table roles_rules (
+    id serial primary key,
+    roles_id int references roles(id),
+    rules_id int references rules(id)
 );
