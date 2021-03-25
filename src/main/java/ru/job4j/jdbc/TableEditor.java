bb42fbc779ab5eef333ff9691b327f8fd9ce0136
Selesito <selesitomorozov@gmail.com>
2021-03-25 12:12:07 +0400

    0.1. Statement [#462090]

---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,99 @@
+package ru.job4j.jdbc;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private Properties properties;
+
+    public TableEditor(Properties properties) throws SQLException, ClassNotFoundException {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        String url = properties.getProperty("url");
+        String login = properties.getProperty("login");
+        String password = properties.getProperty("password");
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    public void createTable(String tableName) {
+        String sql = String.format(
+                "create table if not exists s%(%s, %s);",
+                tableName,
+                "id serial primary key",
+                "name varchar(255)"
+        );
+        execute(sql);
+    }
+
+    public void dropTable(String tableName) {
+        String sql = String.format(
+                "drop table if not exists s%;"
+        );
+        execute(sql);
+    }
+
+    public void addColumn(String tableName, String columnName, String type) {
+        String sql = String.format(
+                "ALTER table if not exists s% ADD s% s%;",
+                tableName,
+                columnName,
+                type
+        );
+        execute(sql);
+    }
+
+    public void dropColumn(String tableName, String columnName) {
+        String sql = String.format(
+                "ALTER table if not exists s% DROP COLUMN s%;",
+                tableName,
+                columnName
+        );
+        execute(sql);
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) {
+        String sql = String.format(
+                "ALTER table if not exists s% RENAME COLUMN s% TO s%;",
+                tableName,
+                columnName,
+                newColumnName
+        );
+        execute(sql);
+    }
+
+    private void execute(String sql) {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getScheme(String tableName) throws SQLException {
+        StringBuilder scheme = new StringBuilder();
+        DatabaseMetaData metaData = connection.getMetaData();
+        try (ResultSet columns = metaData.getColumns(null, null, tableName, null)) {
+            scheme.append(String.format("%-15s %-15s%n", "column", "type"));
+            while (columns.next()) {
+                scheme.append(String.format("%-15s %-15s%n",
+                        columns.getString("COLUMN_NAME"),
+                        columns.getString("TYPE_NAME")));
+            }
+        }
+        return scheme.toString();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
